73942eb2361df78a15ad562212dfdc7718ec4360
Cedric <djouyaguengc@gmail.com>
2026-06-28 06:16:29 +0100

    ajout du voice

--- djeson_assistant/src/config.js
+++ djeson_assistant/src/config.js
@@ -11,10 +11,10 @@ export const PROFILE_IMAGE = profilImg;
 export const ASSISTANT_NAME = "Djeson Pro";
 
 export const WELCOME_MESSAGE =
-  "Bienvenue chez Djeson pronostique ! 👋 Je suis son assitant, quelle est votre probleme aujourd'hui";
+  "Bienvenue chez Djeson pronostique ! 👋 Je suis son assitant, ecoute d'abord le voice suivant avant de poser ta question";
 
 // Remplacer par le chemin de votre fichier audio (ex: "/welcome.mp3") ou une URL
-export const AUDIO_URL = "/welcome.mp3";
+export const AUDIO_URL = "/welcome.ogg";
 
 // Remplacer par votre numéro WhatsApp international (sans le +)
 export const WHATSAPP_URL = "https://whatsapp.com/channel/0029VbBa5zMG3R3jQtowtx2M";
